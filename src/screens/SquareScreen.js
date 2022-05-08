db961f941e5da93ce4c9e419a6643574eafea764
RajMazing <caseyraj92@gmail.com>
2022-05-07 22:22:07 -0500

    added functionality to increase and decrease buttons for the color red

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 
 
 
 const SquareScreen = () => {
-  return <Text> Square Screen</Text>;
+  const [ red, setRed] = useState(0);
+  const [ green, setGreen] = useState(0);
+  const [ blue, setBlue] = useState(0);
+
+      
+  return ( 
+    <View>
+      <ColorCounter 
+      onIncrease={() => setRed(red + 1)} 
+      onDecrease={() => setRed(red - 1)}
+      color="Red" />
+      <ColorCounter color="Blue" />
+      <ColorCounter color="Green" />
+    </View>
+  )
 }
 
 
