ff6d73e30894b135124ff8eee6e2ff86996e6702
RajMazing <caseyraj92@gmail.com>
2022-05-07 21:17:54 -0500

    added a colorScreen with object that generates a new color when refreshed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -19,6 +19,9 @@ const HomeScreen = ({ navigation }) => {
        <Button 
        title="Go to Counter Demo"
        onPress={() => navigation.navigate('Counter')} />
+       <Button 
+       title="Go to Color Demo"
+       onPress={() => navigation.navigate('Color')} />
   </View>
 
   ) 
