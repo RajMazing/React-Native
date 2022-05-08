96f92846440c9173379f0dbfca70582b41c37d00
RajMazing <caseyraj92@gmail.com>
2022-05-08 00:35:39 -0500

    finished refactoring the reducer function, adding branch to main

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 15;
+
 const reducer = (state, action) => {
   // state === { red: number, blue: number, green: number }
   // action === { colorToChange: 'red' || 'blue' || 'green', amount: 15 || -15 }
@@ -11,11 +12,17 @@ const reducer = (state, action) => {
     case 'red':
       //dont change state objects directly
       //state.red = state.red - 15;
-      return {...state, red: state.red + action.amount };
+    return state.red + action.amount > 255 || state.red + action .amount < 0
+         ? state
+         : {...state, red: state.red + action.amount };
     case 'blue':
-      return {...state, blue: state.blue + action.amount };
+      return state.blue + action.amount > 255 || state.blue + action .amount < 0
+         ? state
+         :  {...state, blue: state.blue + action.amount };
     case 'green':
-      return {...state, green: state.green + action.amount };
+      return state.green + action.amount > 255 || state.green + action .amount < 0
+         ? state
+         : {...state, green: state.green + action.amount };
       default:  
         return state; 
   }
