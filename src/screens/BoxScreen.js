8fa77768dce42508453248b493d93f639eee130a
RajMazing <caseyraj92@gmail.com>
2022-05-10 15:30:41 -0500

    made examples for flexbox, position and layout

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,46 +1,77 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Position from 'react-native/Libraries/Components/Touchable/Position';
 
 
 
 
 const BoxScreen = () => {
   return (
-      <View style={styles.viewStyle}>
-        <Text style={styles.textOneStyle}>Child #1</Text>
-        <Text style={styles.textTwoStyle}>Child #2</Text>
-        <Text style={styles.textThreeStyle}>Child #3</Text>
-      </View>
-  )
+      // <View style={styles.viewStyle}>
+      //   <Text style={styles.textOneStyle}>Child #1</Text>
+      //   <Text style={styles.textTwoStyle}>Child #2</Text>
+      //   <Text style={styles.textThreeStyle}>Child #3</Text>
+      // </View>
+
+      <View style={styles.parentStyle}>
+          <View style={styles.viewOneStyle}></View>
+          <View style={styles.viewTwoStyle}></View>
+          <View style={styles.viewThreeStyle}></View>
+          </View>
+  )  
 }
 
 const styles = StyleSheet.create({
-    viewStyle: {
+    // viewStyle: {
+    //   borderWidth: 3,
+    //   borderColor: 'black',
+    //   height: 200,
+    
+     
+    // },
+    // textOneStyle: {
+    //   borderWidth: 3,
+    //   borderColor: 'red'
+  
+     
+    // },
+    // textTwoStyle: {
+    //   borderWidth: 3,
+    //   borderColor: 'red',
+    //   fontSize: 18,
+    //   ...StyleSheet.absoluteFillObject
+    //   // The same as top: value, right: value, bottom: value, left: value
+    // },
+    // textThreeStyle: {
+    //   borderWidth: 3, 
+    //   borderColor: 'red',
+     
+    // }
+
+    parentStyle: {
       borderWidth: 3,
       borderColor: 'black',
       height: 200,
-    
-     
+      flexDirection: 'row',
+      justifyContent: 'space-between'
     },
-    textOneStyle: {
-      borderWidth: 3,
-      borderColor: 'red'
-  
-     
+    viewOneStyle: {
+      height: 50,
+      width: 50,
+      backgroundColor: 'red'
     },
-    textTwoStyle: {
-      borderWidth: 3,
-      borderColor: 'red',
-      fontSize: 18,
-      ...StyleSheet.absoluteFillObject
-      // The same as top: value, right: value, bottom: value, left: value
+    viewTwoStyle: {
+      height: 50,
+      width: 50,
+      backgroundColor: 'green',
+      marginTop: 50
     },
-    textThreeStyle: {
-      borderWidth: 3, 
-      borderColor: 'red',
-     
+    viewThreeStyle: {
+      height: 50, 
+      width: 50,
+      backgroundColor: 'orange'
     }
-
+   
 });
 
 export default BoxScreen;
