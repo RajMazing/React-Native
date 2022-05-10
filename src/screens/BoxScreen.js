a97b7d4cb8b7ddaf825a810bc773339aa7ee4ef5
RajMazing <caseyraj92@gmail.com>
2022-05-10 14:50:57 -0500

    added StyleSheet.absoluteFillObject in textTwoStyle object CSS

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -7,20 +7,38 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
       <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Box Screen</Text>
+        <Text style={styles.textOneStyle}>Child #1</Text>
+        <Text style={styles.textTwoStyle}>Child #2</Text>
+        <Text style={styles.textThreeStyle}>Child #3</Text>
       </View>
   )
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
-      borderWidth: 1,
-      borderColor: 'black'
+      borderWidth: 3,
+      borderColor: 'black',
+      height: 200,
+    
+     
     },
-    textStyle: {
-      borderWidth: 1,
+    textOneStyle: {
+      borderWidth: 3,
+      borderColor: 'red'
+  
+     
+    },
+    textTwoStyle: {
+      borderWidth: 3,
       borderColor: 'red',
-      margin: 20
+      fontSize: 18,
+      ...StyleSheet.absoluteFillObject
+      // The same as top: value, right: value, bottom: value, left: value
+    },
+    textThreeStyle: {
+      borderWidth: 3, 
+      borderColor: 'red',
+     
     }
 
 });
